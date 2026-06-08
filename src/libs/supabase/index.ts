e3d7b883c51b564b37b46e194f2bbfb7e4c98c33
Youssef <youssef.benarab@hotmail.com>
2026-06-08 21:43:41 +0200

    feat: add type annotation to createClient function and improve environment variable checks

--- src/libs/supabase/index.ts
+++ src/libs/supabase/index.ts
@@ -3,11 +3,18 @@ import type { SupabaseClient } from "@supabase/supabase-js";
 
 let browserClient: SupabaseClient | null = null;
 
-export const createClient = () => {
+export const createClient = (): SupabaseClient | null => {
+  if (
+    !process.env.NEXT_PUBLIC_SUPABASE_URL ||
+    !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  ) {
+    return null;
+  }
+
   if (!browserClient) {
     browserClient = createBrowserClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      process.env.NEXT_PUBLIC_SUPABASE_URL,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     );
   }
 
